5f49f43e22e9a47ea786603ed8f124e7da09538e
oriel.zaken <orielux@gmail.com>
2016-01-17 15:07:19 +0200

    Index.html changes, package,json changes

--- src/common/directives/jump-size.validator.directive.js
+++ src/common/directives/jump-size.validator.directive.js
@@ -3,9 +3,9 @@
  */
 (function(app) {
 
-    app.directive('jumpSize', ['$timeout', jumpSize]);
+    app.directive('step', ['$timeout', step]);
 
-    function jumpSize($timeout) {
+    function step($timeout) {
 
         return {
             require: 'ngModel',
@@ -14,49 +14,55 @@
 
         function link(scope, elem, attrs, ngModel) {
 
-            var jumpSize = attrs.jumpSize;
+            var step = attrs.step;
 
-            scope.$watch(attrs.ngModel, function (newVal, oldVal) {
-
-                if (!newVal || !oldVal || newVal % jumpSize === 0) {
-                    return;
-                }
-
-                var fixedValue = newVal;
-
-                // Value increased
-                if (newVal > oldVal) {
-                    for (var i = 0; i < jumpSize; i++) {
-
-                        fixedValue++;
-
-                        if (fixedValue % jumpSize === 0) {
-                            break;
-                        }
-                    }
-                }
-
-                // Value decreased
-                if (newVal < oldVal) {
-                    for (var i = jumpSize; i > 0; i--) {
-
-                        fixedValue--;
-
-                        if (fixedValue % jumpSize === 0) {
-                            break;
-                        }
-                    }
-                }
-
-                $timeout(function() {
-                    ngModel.$setViewValue(fixedValue);
-                    ngModel.$render();
-                },0);
-
-            });
+            //scope.$watch(attrs.ngModel, function (newVal, oldVal) {
+            //
+            //    //if (!newVal || !oldVal || newVal % step === 0) {
+            //    //    return;
+            //    //}
+            //
+            //    if (!newVal)
+            //        return;
+            //
+            //    var fixedValue = newVal;
+            //
+            //    // Value increased
+            //    if (newVal > oldVal) {
+            //        for (var i = 0; i < step; i++) {
+            //
+            //            fixedValue++;
+            //
+            //            if (fixedValue % step === 0) {
+            //                break;
+            //            }
+            //        }
+            //    }
+            //
+            //    // Value decreased
+            //    if (newVal < oldVal) {
+            //        for (var i = step; i > 0; i--) {
+            //
+            //            fixedValue--;
+            //
+            //            if (fixedValue % step === 0) {
+            //                break;
+            //            }
+            //        }
+            //    }
+            //
+            //    if (fixedValue < step)
+            //        fixedValue = step;
+            //
+            //    $timeout(function() {
+            //        ngModel.$setViewValue(fixedValue);
+            //        ngModel.$render();
+            //    },0);
+            //
+            //});
 
             ngModel.$parsers.unshift(function (value) {
-                ngModel.$setValidity('jumpsize', value % jumpSize === 0);
+                ngModel.$setValidity('step', (value / step) % 1 === 0);
                 return value;
             });
         }
