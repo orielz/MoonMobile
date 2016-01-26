88f5b11a69633dc30569674ce755d8b3e629f039
kfir.gershon <Gershon.kf@gmail.com>
2016-01-26 15:58:34 +0200

    navBar serves return navigation model

--- src/components/nav-bar/nav-bar.service.js
+++ src/components/nav-bar/nav-bar.service.js
@@ -1,53 +1,98 @@
-/**
- * Created by oriel.zaken on 11/30/2015.
- */
 (function (app) {
     'use strict';
 
     var serviceId = 'navbarService';
 
-    app.factory(serviceId, ['constants', '$http', '$q', '$localStorage', navbarService]);
+    app.factory(serviceId, [navbarService]);
 
-    function navbarService(constants, $http, $q, $localStorage) {
+    function navbarService() {
 
-        var model = {};
+        var navBarModel = {
+            help: []
+        };
 
         return {
             buildSideBar: buildSideBar,
-            getSideBar: getSideBar
+            getNavBar: getNavBar
         };
 
+        /*
+         * Add the current tab to the requested model
+         * @param attribute - {object} the attribute from attributes json
+         * @return undefined
+         */
         function buildSideBar(attributes) {
 
+            var subTabPrefix = "_SUBTABS";
+            var helpPrefix = "HELP_";
             var attributes = attributes.GroupAttributes;
 
-            for(var attr in attributes){
+            for (var attr in attributes) {
+
                 if (!attributes.hasOwnProperty(attr)) {
                     continue;
                 }
 
-                if (_.endsWith(attr, "_SUBTABS")) {
+                var attribute = attributes[attr];
 
-                    var subTabObj = attributes[attr]["AttributeValue"];
-                    console.log(subTabObj);
-                    var subTabValues = [];
+                if (_.endsWith(attr, subTabPrefix))
+                    addTabToModel(attribute);
+                else if (_.startsWith(attr, helpPrefix))
+                    addHelpToModel(attribute);
 
-                    for(var subTabValue in subTabObj){
-                        if (!attributes.hasOwnProperty(attr)) {
-                            continue;
-                        }
-
-                        model[subTabValue] = "Kfir";
-                    }
-                }
             }
-            console.log(model);
-            console.log(attributes);
         }
 
-        function getSideBar() {
+        /*
+         * Add the current tab to the requested navBarModel
+         * @param attribute - {object} the attribute from attributes json that end with "_SUBTABS"
+         * @return undefined
+         */
+        function addTabToModel(attribute) {
+
+            attribute = attribute["AttributeValue"];
+
+            // Iterate over the sent attribute
+            for (var attr in attribute) {
+
+                navBarModel[attr] = [];
+                var subTabs = attribute[attr];
+
+                for (var prop in subTabs) {
+
+                    var item = {
+                        name: subTabs[prop].translateKey,
+                        order: subTabs[prop].order
+                    };
+
+                    navBarModel[attr].push(item);
+                }
+            }
+        }
+
+        /*
+         * Add the current help section to the requested navBarModel
+         * @param attribute - {object} the attribute from attributes json that start with "HELP_"
+         * @return undefined
+         */
+        function addHelpToModel(attribute) {
+
+            var item = {
+                name: attribute.AttributeName,
+                value: attribute.AttributeValue
+            };
+
+            navBarModel.help.push(item);
+        }
+
+        /*
+         * Return the navBarModel Object
+         * @param attribute - undefined
+         * @return - {object} the navigation bar model
+         */
+        function getNavBar() {
             // To return the model
-            return model;
+            return navBarModel;
         }
     }
 
